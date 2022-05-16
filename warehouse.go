b126e2d1196a3e2d264e79b32cb15d2979ac40a1
Artem Hluvchynskyi <excieve@gmail.com>
2022-05-16 15:47:58 +0300

    Implements CD searching

--- warehouse.go
+++ warehouse.go
@@ -1,11 +1,25 @@
 package cdwarehouse
 
-type Warehouse []string
+type Warehouse []Cd
 
-func (w *Warehouse) Search(artist string, title string) []string {
-	return []string{}
+type Cd struct {
+	Artist string
+	Title  string
 }
 
-func NewWarehouse() *Warehouse {
-	return &Warehouse{}
+func (w *Warehouse) Search(artist string, title string) []Cd {
+	var cds = make([]Cd, 0)
+
+	for _, cd := range *w {
+		if cd.Title == title && cd.Artist == artist {
+			cds = append(cds, cd)
+		}
+	}
+
+	return cds
+}
+
+func NewWarehouse(cds []Cd) *Warehouse {
+	warehouse := Warehouse(cds)
+	return &warehouse
 }
--- warehouse_test.go
+++ warehouse_test.go
@@ -8,12 +8,26 @@ import (
 
 func TestCustomerBuyCD(t *testing.T) {
 	t.Run("Customer finds no CD with artist 'Foo' and title 'Bar'", func(t *testing.T) {
-		warehouse := NewWarehouse()
+		warehouse := NewWarehouse([]Cd{})
 
 		assert.NotNil(t, warehouse)
 
 		cds := warehouse.Search("Foo", "Bar")
 
 		assert.Equal(t, 0, len(cds))
+	})
+
+	t.Run("Customer finds one CD with artist 'Foo' and title 'Bar'", func(t *testing.T) {
+		warehouse := NewWarehouse([]Cd{{"Foo", "Bar"}})
+
+		assert.NotNil(t, warehouse)
+
+		cds := warehouse.Search("Foo", "Bar")
+
+		assert.Len(t, cds, 1)
+
+		foundCd := cds[0]
+		assert.Equal(t, "Foo", foundCd.Artist)
+		assert.Equal(t, "Bar", foundCd.Title)
 	})
 }
